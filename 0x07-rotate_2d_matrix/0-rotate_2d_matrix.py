1cc8f6d6f6d6a0590a4bff20e259ab4854952a8f
Jewel <jewelbansah@rocketmail.com>
2024-06-13 21:38:17 +0000

    fix bugs

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
 """
-Rotate 2D Matrix    
+Rotate 2D Matrix
 """
-def rotate_2d_matrix(matrix):
+
+
+def rotate_2d_matrix(matrix: List[List[int]]) -> None:
     """
     Rotate 2D Matrix
+
+    Args:
+        matrix (List[List[int]]): The 2D matrix to be rotated.
+
+    Returns:
+        None
     """
+
     matrix.reverse()
     for i in range(len(matrix)):
         for j in range(i):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-
